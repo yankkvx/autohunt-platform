eb7e43ae1fe476de5bab1c4cf4e4c94ed52bacbb
yankkvx <yankkvx@gmail.com>
2025-08-08 23:14:57 +0300

    chore(settings): configure custom user model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
+
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -95,9 +98,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
