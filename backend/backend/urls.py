bf1d08eaab9135130d9530fdbf1bd33989994aff
yankkvx <yankkvx@gmail.com>
2025-08-11 19:31:50 +0300

    chore: update main urls.py with user urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,6 +6,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/', include('users.urls')),
 ]
-
