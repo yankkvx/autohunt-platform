ae27d14f2dd2f069e641c49d00a010ee7679a2e5
yankkvx <yankkvx@gmail.com>
2025-08-26 16:42:12 +0300

    feat: add custom jwt serializers for user data
    - Created UserSerializer to expose full user profile fields
    - Added UserSerializerRefreshToken to include jwt token in th response
    - Added MyTokenObtainPairSerializer to return user data with jwt tokens

---
+++ backend/account/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'account_type', 'phone_number', 'first_name',
+                  'last_name', 'about', 'profile_image', 'telegram', 'twitter',
+                  'instagram', 'company_name', 'company_website', 'company_office',
+                  'is_active', 'is_staff']
+
+
+class UserSerializerRefreshToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ['token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerRefreshToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        return data
