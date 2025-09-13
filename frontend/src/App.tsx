151d11755058786a95fad5b47fcba00c051936e0
yankkvx <yankkvx@gmail.com>
2025-09-13 14:22:24 +0300

    feat: set up main App component with routing and theme providing
    - Added BrowsserRouter for client-side routing
    - Wrapped app with ColorToggle to provide dark/ligth theme context
    - Prepared app structure for addint additional screens and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { Routes, Route, BrowserRouter } from "react-router";
+import HomeScreen from "./screens/HomeScreen";
+import ColorToggle from "./components/ColorMode/CologToggle";
+
 function App() {
-    return <div>AutoHunt</div>;
+    return (
+        <BrowserRouter>
+            <ColorToggle>
+                <Routes>
+                    <Route path="/" element={<HomeScreen />} />
+                </Routes>
+            </ColorToggle>
+        </BrowserRouter>
+    );
 }
 
 export default App;
