a4f189b13443928f6b2b3e90f05c15bf800f99f7
yankkvx <yankkvx@gmail.com>
2025-09-13 14:21:57 +0300

    chore: created base HomeScreen(empty)

---
+++ frontend/src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Header from "../components/Header/Header";
+
+const HomeScreen = () => {
+    return (
+        <Box sx={{ display: "flex" }}>
+            <Header />
+            HomeScreen
+        </Box>
+    );
+};
+
+export default HomeScreen;
