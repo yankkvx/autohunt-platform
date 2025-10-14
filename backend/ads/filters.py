b5fb29317606322a716fbaf885026562d5eb6144
yankkvx <yankkvx@gmail.com>
2025-10-14 21:33:57 +0300

    feat: add user based filtering support

--- backend/ads/filters.py
+++ backend/ads/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 from .models import Ad
 from catalog.models import Brand, ModelCar, BodyType, FuelType, DriveType, Transmission, Color, InteriorMaterial
+from account.models import User
 
 FOREIGN_FILTERS = {
     'brand': Brand,
@@ -12,31 +13,52 @@ FOREIGN_FILTERS = {
     'exterior_color': Color,
     'interior_color': Color,
     'interior_material': InteriorMaterial,
+    'user': User
 }
 
 
 class AdFilter(django_filters.FilterSet):
     # Ranges for the numeric fields
-    year_min = django_filters.NumberFilter(field_name='year', lookup_expr='gte')
-    year_max = django_filters.NumberFilter(field_name='year', lookup_expr='lte')
-    price_min = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
-    price_max = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
-    mileage_min = django_filters.NumberFilter(field_name='mileage', lookup_expr='gte')
-    mileage_max = django_filters.NumberFilter(field_name='mileage', lookup_expr='lte')
-    power_min = django_filters.NumberFilter(field_name='power', lookup_expr='gte')
-    power_max = django_filters.NumberFilter(field_name='power', lookup_expr='lte')
-    capacity_min = django_filters.NumberFilter(field_name='capacity', lookup_expr='gte')
-    capacity_max = django_filters.NumberFilter(field_name='capacity', lookup_expr='lte')
-    battery_power_min = django_filters.NumberFilter(field_name='battery_power', lookup_expr='gte')
-    battery_power_max = django_filters.NumberFilter(field_name='battery_power', lookup_expr='lte')
-    battery_capacity_min = django_filters.NumberFilter(field_name='battery_capacity', lookup_expr='gte')
-    battery_capacity_max = django_filters.NumberFilter(field_name='battery_capacity', lookup_expr='lte')
-    number_of_seats_min = django_filters.NumberFilter(field_name='number_of_seats', lookup_expr='gte')
-    number_of_seats_max = django_filters.NumberFilter(field_name='number_of_seats', lookup_expr='lte')
-    number_of_doors_min = django_filters.NumberFilter(field_name='number_of_doors', lookup_expr='gte')
-    number_of_doors_max = django_filters.NumberFilter(field_name='number_of_doors', lookup_expr='lte')
-    owner_count_min = django_filters.NumberFilter(field_name='owner_count', lookup_expr='gte')
-    owner_count_max = django_filters.NumberFilter(field_name='owner_count', lookup_expr='lte')
+    year_min = django_filters.NumberFilter(
+        field_name='year', lookup_expr='gte')
+    year_max = django_filters.NumberFilter(
+        field_name='year', lookup_expr='lte')
+    price_min = django_filters.NumberFilter(
+        field_name='price', lookup_expr='gte')
+    price_max = django_filters.NumberFilter(
+        field_name='price', lookup_expr='lte')
+    mileage_min = django_filters.NumberFilter(
+        field_name='mileage', lookup_expr='gte')
+    mileage_max = django_filters.NumberFilter(
+        field_name='mileage', lookup_expr='lte')
+    power_min = django_filters.NumberFilter(
+        field_name='power', lookup_expr='gte')
+    power_max = django_filters.NumberFilter(
+        field_name='power', lookup_expr='lte')
+    capacity_min = django_filters.NumberFilter(
+        field_name='capacity', lookup_expr='gte')
+    capacity_max = django_filters.NumberFilter(
+        field_name='capacity', lookup_expr='lte')
+    battery_power_min = django_filters.NumberFilter(
+        field_name='battery_power', lookup_expr='gte')
+    battery_power_max = django_filters.NumberFilter(
+        field_name='battery_power', lookup_expr='lte')
+    battery_capacity_min = django_filters.NumberFilter(
+        field_name='battery_capacity', lookup_expr='gte')
+    battery_capacity_max = django_filters.NumberFilter(
+        field_name='battery_capacity', lookup_expr='lte')
+    number_of_seats_min = django_filters.NumberFilter(
+        field_name='number_of_seats', lookup_expr='gte')
+    number_of_seats_max = django_filters.NumberFilter(
+        field_name='number_of_seats', lookup_expr='lte')
+    number_of_doors_min = django_filters.NumberFilter(
+        field_name='number_of_doors', lookup_expr='gte')
+    number_of_doors_max = django_filters.NumberFilter(
+        field_name='number_of_doors', lookup_expr='lte')
+    owner_count_min = django_filters.NumberFilter(
+        field_name='owner_count', lookup_expr='gte')
+    owner_count_max = django_filters.NumberFilter(
+        field_name='owner_count', lookup_expr='lte')
 
     # Boolean filters
     warranty = django_filters.BooleanFilter(field_name='warranty')
@@ -63,5 +85,5 @@ class AdFilter(django_filters.FilterSet):
             'interior_color', 'interior_material', 'warranty', 'airbag', 'air_conditioning',
             'is_first_owner', 'number_of_seats_min', 'number_of_seats_max',
             'number_of_doors_min', 'number_of_doors_max', 'owner_count_min', 'owner_count_max',
-            'condition', 'price_min', 'price_max'
+            'condition', 'price_min', 'price_max', 'user',
         ]
