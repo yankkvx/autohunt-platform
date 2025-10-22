501f0b3cad780698bc5df8a9103b5b8d5c3ca306
yankkvx <yankkvx@gmail.com>
2025-10-22 23:45:58 +0300

    chore: configure ASGI for websocket support with jwt authentication
    - Added websocket routing using URLRouter
    - Secured connections with AllowedHostsOriginValidator
    - Implemented custom JWTMiddleware for websocket token authentication

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,16 +1,23 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
+from chat.middleware import JWTMiddleware
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+
+    'websocket': AllowedHostsOriginValidator(
+        JWTMiddleware(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    ),
+})
